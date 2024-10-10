56e662e95e6950684f81ce63595d498840c9bccf
Mike <Mike@192.168.0.3>
2024-10-10 12:43:24 +0530

    project

---
+++ JavapojectAnu/src/main/java/printnumbers/Onetoten.java
@@ -0,0 +1,18 @@
+package printnumbers;
+
+public class Onetoten {
+
+	public static void main(String[] args) {
+		
+		for(int i=1;i<=10;i++)
+		{
+			System.out.println(+i);
+		}
+		
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
